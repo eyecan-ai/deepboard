ce4034e80afe45b31779bb8f7ae517bdc23278ef
domef <federicodomeniconi@gmail.com>
2020-10-23 18:49:46 +0200

    implementation of paper

--- deepboard/board/board.py
+++ deepboard/board/board.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Sequence
 import numpy as np
 from deepboard.corners.order_corners import OrderCorners
 
@@ -29,3 +29,51 @@ class Board(object):
         d = np.linalg.norm(self.corners[3, :] - self.corners[0, :])
 
         return a, b, c, d
+
+    def rectified_ratio(self, img_size: Sequence[int]) -> float:
+        """ Returns the ratio w / h of the rectified board
+
+        Source: https://www.microsoft.com/en-us/research/uploads/prod/2016/11/Digital-Signal-Processing.pdf
+
+        :param img_size: size [h, w] of the original image
+        :type img_size: Sequence[int]
+        :return: ratio w / h of the rectified board
+        :rtype: float
+        """
+
+        u0, v0 = img_size[1] / 2, img_size[0] / 2
+
+        m1 = np.append(self.corners[3], 1)
+        m2 = np.append(self.corners[2], 1)
+        m3 = np.append(self.corners[0], 1)
+        m4 = np.append(self.corners[1], 1)
+
+        k2 = np.dot(np.cross(m1, m4), m3) / np.dot(np.cross(m2, m4), m3)
+        k3 = np.dot(np.cross(m1, m4), m2) / np.dot(np.cross(m3, m4), m2)
+
+        n2 = k2 * m2 - m1
+        n3 = k3 * m3 - m1
+
+        # # handle singularities
+        # if k2 == 1 or k3 == 1:
+        #     ratio = np.sqrt((n2[0] ** 2 + n2[1] ** 2) / (n3[0] ** 2 + n3[1] ** 2))
+        #     return ratio.item()
+
+        f_num1 = n2[0] * n3[0] - (n2[0] * n3[2] + n2[2] * n3[0]) * u0 + n2[2] * n3[2] * u0 ** 2
+        f_num2 = n2[1] * n3[1] - (n2[1] * n3[2] + n2[2] * n3[1]) * v0 + n2[2] * n3[2] * v0 ** 2
+        f_den = n2[2] * n3[2]
+
+        # minus in f ???
+
+        f = np.sqrt(np.abs((f_num1 + f_num2) / f_den))
+
+        A = np.array([[f, 0, u0],
+                      [0, f, v0],
+                      [0, 0, 1]])
+
+        Ati = np.linalg.inv(A.T)
+        Ai = np.linalg.inv(A)
+
+        ratio = np.sqrt(np.linalg.multi_dot((n2.T, Ati, Ai, n2)) / np.linalg.multi_dot((n3.T, Ati, Ai, n3)))
+
+        return ratio.item()
--- examples/correct_board/correct_board2.py
+++ examples/correct_board/correct_board2.py
@@ -8,37 +8,37 @@ from deepboard.markers.rescale import RescaleBoard
 from scipy import spatial
 
 
-def wh_ratio(img, corners):
-    h, w = img.shape[:2]
-    u0, v0 = w / 2, h / 2
+# def wh_ratio(img, corners):
+#     h, w = img.shape[:2]
+#     u0, v0 = w / 2, h / 2
 
-    x1 = corners[3][0]
-    y1 = corners[3][1]
-    x2 = corners[2][0]
-    y2 = corners[2][1]
-    x3 = corners[0][0]
-    y3 = corners[0][1]
-    x4 = corners[1][0]
-    y4 = corners[1][1]
+#     x1 = corners[3][0]
+#     y1 = corners[3][1]
+#     x2 = corners[2][0]
+#     y2 = corners[2][1]
+#     x3 = corners[0][0]
+#     y3 = corners[0][1]
+#     x4 = corners[1][0]
+#     y4 = corners[1][1]
 
-    x1 = x1 - u0
-    y1 = y1 - v0
-    x2 = x2 - u0
-    y2 = y2 - v0
-    x3 = x3 - u0
-    y3 = y3 - v0
-    x4 = x4 - u0
-    y4 = y4 - v0
+#     x1 = x1 - u0
+#     y1 = y1 - v0
+#     x2 = x2 - u0
+#     y2 = y2 - v0
+#     x3 = x3 - u0
+#     y3 = y3 - v0
+#     x4 = x4 - u0
+#     y4 = y4 - v0
 
-    k2 = ((y1 - y4) * x3 - (x1 - x4) * y3 + x1 * y4 - y1 * x4) / ((y2 - y4) * x3 - (x2 - x4) * y3 + x2 * y4 - y2 * x4)
-    k3 = ((y1 - y4) * x2 - (x1 - x4) * y2 + x1 * y4 - y1 * x4) / ((y3 - y4) * x2 - (x3 - x4) * y2 + x3 * y4 - y3 * x4)
-    f_squared = -((k3 * y3 - y1) * (k2 * y2 - y1) + (k3 * x3 - x1) * (k2 * x2 - x1)) / ((k3 - 1) * (k2 - 1))
-    print(k2, k3, f_squared)
-    hw_ratio = math.sqrt((pow(k2 - 1, 2) + pow(k2 * y2 - y1, 2) / f_squared + pow(k2 * x2 - x1, 2) / f_squared) / (pow(k3 - 1, 2) + pow(k3 * y3 - y1, 2) / f_squared + pow(k3 * x3 - x1, 2) / f_squared))
+#     k2 = ((y1 - y4) * x3 - (x1 - x4) * y3 + x1 * y4 - y1 * x4) / ((y2 - y4) * x3 - (x2 - x4) * y3 + x2 * y4 - y2 * x4)
+#     k3 = ((y1 - y4) * x2 - (x1 - x4) * y2 + x1 * y4 - y1 * x4) / ((y3 - y4) * x2 - (x3 - x4) * y2 + x3 * y4 - y3 * x4)
+#     f_squared = -((k3 * y3 - y1) * (k2 * y2 - y1) + (k3 * x3 - x1) * (k2 * x2 - x1)) / ((k3 - 1) * (k2 - 1))
+#     print(k2, k3, f_squared)
+#     hw_ratio = math.sqrt((pow(k2 - 1, 2) + pow(k2 * y2 - y1, 2) / f_squared + pow(k2 * x2 - x1, 2) / f_squared) / (pow(k3 - 1, 2) + pow(k3 * y3 - y1, 2) / f_squared + pow(k3 * x3 - x1, 2) / f_squared))
 
-    # hw_ratio2 = math.sqrt((pow(y2 - y1, 2) + pow(x2 - x1, 2)) / (pow(y3 - y1, 2) + pow(x3 - x1, 2)))
+#     # hw_ratio2 = math.sqrt((pow(y2 - y1, 2) + pow(x2 - x1, 2)) / (pow(y3 - y1, 2) + pow(x3 - x1, 2)))
 
-    return 1 / hw_ratio
+#     return 1 / hw_ratio
 
 
 def get_corners(event, x, y, flags, params):
@@ -49,82 +49,139 @@ def get_corners(event, x, y, flags, params):
         params[0].append([x, y])
 
 
-def wh_ratio2(img, corners):
-    (rows,cols,_) = img.shape
+# def wh_ratio2(img, corners):
+#     (rows,cols,_) = img.shape
 
-    #image center
-    u0 = (cols)/2.0
-    v0 = (rows)/2.0
+#     #image center
+#     u0 = (cols)/2.0
+#     v0 = (rows)/2.0
 
-    x1 = corners[3][0]
-    y1 = corners[3][1]
-    x2 = corners[2][0]
-    y2 = corners[2][1]
-    x3 = corners[0][0]
-    y3 = corners[0][1]
-    x4 = corners[1][0]
-    y4 = corners[1][1]
+#     # x1 = corners[3][0] - u0
+#     # y1 = corners[3][1] - v0
+#     # x2 = corners[2][0] - u0
+#     # y2 = corners[2][1] - v0
+#     # x3 = corners[0][0] - u0
+#     # y3 = corners[0][1] - v0
+#     # x4 = corners[1][0] - u0
+#     # y4 = corners[1][1] - v0
 
-    #detected corners on the original image
-    p = []
-    p.append((x1,y1))
-    p.append((x2,y2))
-    p.append((x3,y3))
-    p.append((x4,y4))
+#     x1 = corners[3][0]
+#     y1 = corners[3][1]
+#     x2 = corners[2][0]
+#     y2 = corners[2][1]
+#     x3 = corners[0][0]
+#     y3 = corners[0][1]
+#     x4 = corners[1][0]
+#     y4 = corners[1][1]
 
-    #widths and heights of the projected image
-    w1 = spatial.distance.euclidean(p[0],p[1])
-    w2 = spatial.distance.euclidean(p[2],p[3])
+#     #detected corners on the original image
+#     p = []
+#     p.append((x1,y1))
+#     p.append((x2,y2))
+#     p.append((x3,y3))
+#     p.append((x4,y4))
 
-    h1 = spatial.distance.euclidean(p[0],p[2])
-    h2 = spatial.distance.euclidean(p[1],p[3])
+#     #widths and heights of the projected image
+#     # w1 = spatial.distance.euclidean(p[0],p[1])
+#     # w2 = spatial.distance.euclidean(p[2],p[3])
 
-    w = max(w1,w2)
-    h = max(h1,h2)
+#     # h1 = spatial.distance.euclidean(p[0],p[2])
+#     # h2 = spatial.distance.euclidean(p[1],p[3])
 
-    #visible aspect ratio
-    ar_vis = float(w)/float(h)
+#     # w = max(w1,w2)
+#     # h = max(h1,h2)
 
-    #make numpy arrays and append 1 for linear algebra
-    m1 = np.array((p[0][0],p[0][1],1)).astype('float32')
-    m2 = np.array((p[1][0],p[1][1],1)).astype('float32')
-    m3 = np.array((p[2][0],p[2][1],1)).astype('float32')
-    m4 = np.array((p[3][0],p[3][1],1)).astype('float32')
+#     #visible aspect ratio
+#     # ar_vis = float(w)/float(h)
 
-    #calculate the focal disrance
-    k2 = np.dot(np.cross(m1,m4),m3) / np.dot(np.cross(m2,m4),m3)
-    k3 = np.dot(np.cross(m1,m4),m2) / np.dot(np.cross(m3,m4),m2)
+#     #make numpy arrays and append 1 for linear algebra
+#     m1 = np.array((p[0][0],p[0][1],1)).astype('float32')
+#     m2 = np.array((p[1][0],p[1][1],1)).astype('float32')
+#     m3 = np.array((p[2][0],p[2][1],1)).astype('float32')
+#     m4 = np.array((p[3][0],p[3][1],1)).astype('float32')
+#     # print(m1)
+#     # print(m2)
+#     # print(m3)
+#     # print(m4)
 
-    n2 = k2 * m2 - m1
-    n3 = k3 * m3 - m1
+#     #calculate the focal disrance
+#     k2 = np.dot(np.cross(m1,m4),m3) / np.dot(np.cross(m2,m4),m3)
+#     k3 = np.dot(np.cross(m1,m4),m2) / np.dot(np.cross(m3,m4),m2)
+#     # print(k2)
+#     # print(k3)
+#     n2 = k2 * m2 - m1
+#     n3 = k3 * m3 - m1
 
-    n21 = n2[0]
-    n22 = n2[1]
-    n23 = n2[2]
+#     n21 = n2[0]
+#     n22 = n2[1]
+#     n23 = n2[2]
 
-    n31 = n3[0]
-    n32 = n3[1]
-    n33 = n3[2]
+#     n31 = n3[0]
+#     n32 = n3[1]
+#     n33 = n3[2]
 
-    f = math.sqrt(np.abs( (1.0/(n23*n33)) * ((n21*n31 - (n21*n33 + n23*n31)*u0 + n23*n33*u0*u0) + (n22*n32 - (n22*n33+n23*n32)*v0 + n23*n33*v0*v0))))
+#     f = math.sqrt(np.abs( (1.0/(n23*n33)) * ((n21*n31 - (n21*n33 + n23*n31)*u0 + n23*n33*u0*u0) + (n22*n32 - (n22*n33+n23*n32)*v0 + n23*n33*v0*v0))))
+#     # print('f', f)
+#     A = np.array([[f,0,u0],[0,f,v0],[0,0,1]]).astype('float32')
 
-    A = np.array([[f,0,u0],[0,f,v0],[0,0,1]]).astype('float32')
+#     At = np.transpose(A)
+#     Ati = np.linalg.inv(At)
+#     Ai = np.linalg.inv(A)
 
-    At = np.transpose(A)
-    Ati = np.linalg.inv(At)
-    Ai = np.linalg.inv(A)
+#     #calculate the real aspect ratio
+#     ar_real = math.sqrt(np.dot(np.dot(np.dot(n2,Ati),Ai),n2)/np.dot(np.dot(np.dot(n3,Ati),Ai),n3))
 
-    #calculate the real aspect ratio
-    ar_real = math.sqrt(np.dot(np.dot(np.dot(n2,Ati),Ai),n2)/np.dot(np.dot(np.dot(n3,Ati),Ai),n3))
+#     # if ar_real < ar_vis:
+#     #     W = int(w)
+#     #     H = int(W / ar_real)
+#     # else:
+#     #     H = int(h)
+#     #     W = int(ar_real * H)
 
-    if ar_real < ar_vis:
-        W = int(w)
-        H = int(W / ar_real)
-    else:
-        H = int(h)
-        W = int(ar_real * H)
+#     return ar_real
 
-    return ar_real
+
+# def my_ratio(img, corners):
+#     h, w = img.shape[:2]
+#     u0, v0 = w / 2, h / 2
+
+#     m1 = np.append(corners[3], 1)
+#     m2 = np.append(corners[2], 1)
+#     m3 = np.append(corners[0], 1)
+#     m4 = np.append(corners[1], 1)
+#     # print(m1)
+#     # print(m2)
+#     # print(m3)
+#     # print(m4)
+
+#     k2 = np.dot(np.cross(m1, m4), m3) / np.dot(np.cross(m2, m4), m3)
+#     k3 = np.dot(np.cross(m1, m4), m2) / np.dot(np.cross(m3, m4), m2)
+#     # print(k2)
+#     # print(k3)
+#     # f = math.sqrt(np.abs((1.0/(n23 * n33)) * ((n21 * n31 - (n21 * n33 + n23 * n31) * u0 + n23 * n33*u0*u0) + (n22 * n32 - (n22 * n33 + n23 * n32) * v0 + n23 * n33 * v0 * v0))))
+
+#     n2 = k2 * m2 - m1
+#     n3 = k3 * m3 - m1
+
+#     f_num1 = n2[0] * n3[0] - (n2[0] * n3[2] + n2[2] * n3[0]) * u0 + n2[2] * n3[2] * u0 ** 2
+#     f_num2 = n2[1] * n3[1] - (n2[1] * n3[2] + n2[2] * n3[1]) * v0 + n2[2] * n3[2] * v0 ** 2
+#     f_den = n2[2] * n3[2]
+#     f = np.sqrt(np.abs((f_num1 + f_num2) / f_den))
+#     # print('my f', f)
+
+
+
+#     A = np.array([[f, 0, u0],
+#                   [0, f, v0],
+#                   [0, 0, 1]])
+
+#     # At = np.transpose(A)
+#     Ati = np.linalg.inv(A.T)
+#     Ai = np.linalg.inv(A)
+
+#     ratio = np.sqrt(np.linalg.multi_dot((n2.T, Ati, Ai, n2)) / np.linalg.multi_dot((n3.T, Ati, Ai, n3)))
+
+#     return ratio
 
 
 @click.command("")
@@ -144,16 +201,19 @@ def run(img, output_width, markers):
         cv2.imshow('board', img_copy)
         cv2.waitKey(1)
 
-    # ratio = wh_ratio(img, corners)
+    # ratio = wh_ratio2(img, corners)
     # print('ratio', ratio)
-    ratio = wh_ratio2(img, corners)
-    print('ratio', ratio)
+    # ratio2 = my_ratio(img, corners)
+    # print('ratio2', ratio2)
 
     if markers is not None:
         markers = np.loadtxt(markers)[:, 1:] * [w, h, w, h]
         board = Board(np.array(corners), markers)
     else:
         board = Board(np.array(corners))
+
+    ratio = board.rectified_ratio(img.shape[:2])
+    print('ratio', ratio)
 
     # compute new board size
     a, b, c, d = board.size
